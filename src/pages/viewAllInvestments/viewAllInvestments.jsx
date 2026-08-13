4a43f1ca6463abc9a16c40fd70ba66e568b47e12
ahmed <salmoahmad079@gmail.com>
2026-08-13 20:32:25 +0300

    also realize filtering but inside view all page

--- src/pages/viewAllInvestments/viewAllInvestments.jsx
+++ src/pages/viewAllInvestments/viewAllInvestments.jsx
@@ -15,8 +15,10 @@ export default function viewAllInvestments(){
 
     const [searchData, setSearchData] = useState(null)
     const [isSearching, setIsSearching] = useState(false);
+    const [isInnerSearching, setIsInnerSearching]= useState(false)
 
     const [dataAfterFilter, setDataAfterFilter] = useState([]);
+    const [dataAfterSearch, setDataAfterSearch] = useState([]);
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -32,19 +34,18 @@ export default function viewAllInvestments(){
 
                 const formData= location.state?.formData
                 formData === undefined ? null : setIsSearching(true)
-                const filteredData = response.data.data.filter(item => {
-                    const locationTest = item.location.toLowerCase() === formData.location.toLowerCase();
-                    const moneyTest = parseFloat( item.total_budget ) <= parseFloat(formData.budget)
-                    return locationTest && moneyTest;
-                    // return item.location.toLowerCase() === (formData).location.toLowerCase();
-                })
-                setDataAfterFilter(filteredData)
+                if(formData !== undefined){
+                    const filteredData = response.data.data.filter(item => {
+                        const locationTest = item.location.toLowerCase() === formData.location.toLowerCase();
+                        const moneyTest = parseFloat( item.total_budget ) <= parseFloat(formData.budget)
+                        return locationTest && moneyTest;
+                        // return item.location.toLowerCase() === (formData).location.toLowerCase();
+                    })
+                    setDataAfterFilter(filteredData)
+                }
             }
             getData();
-            
-            // const formData= location.state?.formData
-            // setSearchData(formData);
-            // (location.state?.formData) === undefined ? null: filteringData();
+
         }catch(error){
             console.log(error)
         }
@@ -57,39 +58,63 @@ export default function viewAllInvestments(){
         location:'',
         money:'',
     })
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
     const searchHandler = ()=>{
-        // const filteredData = data.filter(item => {
-        //     const locationMatch = SearchValue.location ? item.location.toLowerCase().includes(SearchValue.location.toLowerCase()) : true;
-        //     const moneyMatch = SearchValue.money ? item.money <= parseFloat(SearchValue.money) : true;
-        //     return locationMatch && moneyMatch;
-        // });
-        // setSearchData(filteredData);
+        if( SearchValue.location === '' && SearchValue.money === '' ){
+            alert('please enter location or money');
+        }
+        else {
+            console.log(data)
+            console.log(SearchValue)
+            const filteredData = data.filter(item => {
+                const locationTest = item.location.toLowerCase() === SearchValue.location.toLowerCase();
+                const moneyTest = parseFloat( item.total_budget ) <= parseFloat(SearchValue.money)
+                return locationTest && moneyTest;
+            })
+            console.log(filteredData);
+
+            return(
+                <div className='ahm-InvestmentOpportunitesList '>
+    
+                    {
+                        filteredData.length>0 && filteredData.map((investment)=>(
+                            <div className='ahm-card ' key={investment.id}> 
+                                {/* <img src={investment.image} alt='invest img' className='ahm-imageCard  '/>  */}
+                                <div className=' ahm-infoCard '>
+                                    <h3 className='ah-h3-card'>{investment.name}</h3>
+                                </div>
+                                <p className='ahm-investLocation '> <FaMapMarkerAlt /> {investment.location}</p>
+                                
+                                <hr className='ahm-line  '/>
+                                
+                                    <p className=' ahm-investmentCost'>{investment.total_budget} $</p>
+                                    
+                                    <button  className='ahm-buyBTN'>
+                                        Buy
+                                    </button>
+                                
+                            </div>
+                        ))
+                    }
+                    
+                    </div>
+            );
+        }
     }
 
-    // const filteringData =()=>{
-    //     const filteredData = data.filter(item => {
-    //         // const locationTest = item.location.toLowerCase() === searchData.location.toLowerCase();
-    //         // const moneyTest = parseFloat( item.total_budget ) <= parseFloat(searchData.budget)
-    //         // return locationTest && moneyTest;
-    //         return item.location.toLowerCase() === searchData.location.toLowerCase();
-    //     })
-    //     console.log(filteredData)
-    //     setDataAfterFilter(filteredData);
-    // }
 
     const SearchForm=()=>{
         return(
             <div className='ahm-formContainerVeiwAll'>
                 <div className='ahm-searchContainer '>
                     <input onChange={e=>setSearchValue({...SearchValue, money:e.target.value})} className='ahm-input ' placeholder='money...' />
-                    <div onClick={ searchHandler} className='ahm-containerSearchIcon'> <FaSearch className='ahm-searchIcon'/> </div>
+                    <div onClick={()=> (searchHandler, setIsInnerSearching(true)) } className='ahm-containerSearchIcon'> <FaSearch className='ahm-searchIcon'/> </div>
                 </div>
                 <select onChange={(e)=>setSearchValue({...SearchValue, location:e.target.value})} className='ahm-selectLocation '>
                     <option value=''>select Location</option>
                     <option value='lattakia'>Lattakia</option>
                     <option value='aleppo'>Aleppo</option>
-                    <option value='damascuse'>Damascuse</option>
+                    <option value='damascus'>Damascus</option>
                 </select> <br/>
                 
             </div>
@@ -162,7 +187,9 @@ export default function viewAllInvestments(){
             </div>
             <hr className='ahm-lineInViewAll'/>
             { searchData == null ? SearchForm():''}
-            {isSearching == true ?  filteredData():InvestmentOpportunitesList() }
+            {isSearching == true ?  filteredData():'' }
+            {isInnerSearching == true ? searchHandler():''}
+            {isInnerSearching == false && isSearching == false ? InvestmentOpportunitesList():''}
         </div>
     )
 }
